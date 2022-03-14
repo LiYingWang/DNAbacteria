ca2c3e4906627a71fc512179d34c7f99dfd559de
Liying Wang <jaybin502@gmail.com>
2022-03-13 23:08:02 -0700

    combine three gene plots

--- DNA-plot.R
+++ DNA-plot.R
@@ -18,6 +18,8 @@ select_RNA_DNA <-
          `Log(FPKM+1)/estrone`,
          `steroid catabolic function.y`) %>%
   mutate(function_group = ifelse(is.na(`steroid catabolic function.y`), "Y", "N")) %>%
+  #mutate(`steroid catabolic function.y` = ifelse(is.na(`steroid catabolic function.y`),
+                                                 #"others",`steroid catabolic function.y`)) %>%
   mutate(selected_DNA =  ifelse(Prokka_ID %in% c("GMFMDNLD_02935",
                                                  "GMFMDNLD_03019"), # specify data point here
                                 Prokka_ID, NA))
@@ -39,9 +41,10 @@ select_RNA_DNA %>%
   theme(legend.position="top")
 
 # use viridis
+plot_ct <-
 select_RNA_DNA %>%
   ggplot(aes(`Log(FPKM+1)/cholesterol`, `Log(FPKM+1)/testosterone`)) +
-  geom_point(color = "gray") +
+  geom_point(color = "darkgray", alpha = 0.5) +
   geom_point(aes(color = `steroid catabolic function.y`),
              size = 2.5,
              alpha = 0.8) +
@@ -57,3 +60,52 @@ select_RNA_DNA %>%
   theme(legend.position="top") +
   theme(legend.title=element_blank())
 
+plot_te <-
+  select_RNA_DNA %>%
+  ggplot(aes(`Log(FPKM+1)/testosterone`, `Log(FPKM+1)/estrone`)) +
+  geom_point(color = "darkgray", alpha = 0.5) +
+  geom_point(aes(color = `steroid catabolic function.y`),
+             size = 2.5,
+             alpha = 0.8) +
+  #geom_label_repel(aes(label = selected_DNA)) +
+  #box.padding   = 0.35,
+  #point.padding = 0.5,
+  #segment.color = 'grey50') +
+  viridis::scale_color_viridis(discrete = TRUE) +
+  geom_point(data = subset(select_RNA_DNA, !is.na(selected_DNA)),
+             col = "red", stroke = 0.8, shape = 21) +
+  guides(alpha = FALSE) +
+  theme_minimal() +
+  theme(legend.position="none")
+
+plot_ce <-
+  select_RNA_DNA %>%
+  ggplot(aes(`Log(FPKM+1)/cholesterol`, `Log(FPKM+1)/estrone`)) +
+  geom_point(color = "darkgray", alpha = 0.5) +
+  geom_point(aes(color = `steroid catabolic function.y`),
+             size = 2.5,
+             alpha = 0.8) +
+  #geom_label_repel(aes(label = selected_DNA)) +
+  #box.padding   = 0.35,
+  #point.padding = 0.5,
+  #segment.color = 'grey50') +
+  viridis::scale_color_viridis(discrete = TRUE) +
+  geom_point(data = subset(select_RNA_DNA, !is.na(selected_DNA)),
+             col = "red", stroke = 0.8, shape = 21) +
+  guides(alpha = FALSE) +
+  theme_minimal() +
+  theme(legend.position="none")
+
+library(cowplot)
+plot_grid(plot_ct,
+          plot_ce,
+          plot_te,
+          labels = c('A', 'B', 'C'),
+          ncol = 1,
+          rel_heights = c(1.3, 1,1))
+
+ggsave(here::here("gene_expression.png"),
+       width = 230,
+       height = 230,
+       dpi = 300,
+       units = "mm")
